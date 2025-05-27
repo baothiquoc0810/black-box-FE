564e4fe7d6d4d65de04645dbb6cc8353b5b45fcf
baothiquoc <bquoc3002@gmail.com>
2025-05-27 16:11:09 +0700

    feat/add-expiration-time

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 import { REGISTER_API_URL, LOGIN_API_URL } from '../constants/apiUrls';
 
+const TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 24; // 24 hours
+
+const saveUserData = (userData) => {
+    const dataWithExpiration = {
+        ...userData,
+        tokenExpiration: Date.now() + TOKEN_EXPIRATION_TIME
+    }
+    localStorage.setItem('user', JSON.stringify(dataWithExpiration));
+    return dataWithExpiration;
+}
+
 const AuthService = {
 
     login: async (username, password) => {
@@ -9,9 +20,9 @@ const AuthService = {
                 username,
                 password
             });
-
+            
             if (response.data.result.token){
-                localStorage.setItem('user', JSON.stringify(response.data.result.user));
+                return saveUserData(response.data.result.user);
             }
             return response.data.result.user;
         } catch (error) {
@@ -24,7 +35,15 @@ const AuthService = {
     },
 
     getCurrentUser: () => {
-        return JSON.parse(localStorage.getItem('user'));
+        const user = JSON.parse(localStorage.getItem('user'));
+        if(!user) return null;
+
+        const currentTime = Date.now();
+        if(currentTime > user.tokenExpiration){
+            localStorage.removeItem('user');
+            return null;
+        }
+        return user;
     },
 
     isAuthenticated: () => {
@@ -40,8 +59,8 @@ const AuthService = {
                 password
             });
             
-            if(response.data.result.token){
-                localStorage.setItem('user', JSON.stringify(response.data.result));
+            if(response.data.result.token) {
+                return AuthService._saveUserData(response.data.result);
             }
             return response.data.result;
         } catch (error) {
